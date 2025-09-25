eb5c939152ebca1b61fb12f7002276d0e19a99f3
Bernapai <juanemeunier@gmail.com>
2025-09-25 19:38:59 -0300

    Add Basic Stripe

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -7,4 +7,33 @@ export class PaymentsService {
     private readonly stripe = new Stripe(
         envs.stripeSecret
     )
+
+    async createPaymentSession() {
+        const session = await this.stripe.checkout.sessions.create({
+            payment_intent_data: {
+                metadata: {
+                },
+            },
+
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'T-shirt',
+                        },
+                        unit_amount: 2000,
+
+                    },
+                    quantity: 1,
+
+                }
+            ],
+            mode: 'payment',
+            success_url: 'https://localhost:3003/payments/success',
+            cancel_url: 'https://localhost:3003/payments/cancel',
+        })
+        return { url: session.url }
+    }
 }
+
